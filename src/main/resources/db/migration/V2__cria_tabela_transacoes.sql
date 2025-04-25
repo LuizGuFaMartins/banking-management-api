a615fac3cc831f56015c80f0637b825f61351538
LuizGuFaMartins <luizgufamartins@gmail.com>
2025-04-25 14:09:13 -0300

    fix: forma de pagamento na tabela de transações

--- src/main/resources/db/migration/V2__cria_tabela_transacoes.sql
+++ src/main/resources/db/migration/V2__cria_tabela_transacoes.sql
@@ -3,7 +3,7 @@ CREATE TABLE
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         conta_id UUID NOT NULL,
         valor NUMERIC(18, 2) NOT NULL,
-        forma_pagamento VARCHAR(1) NOT NULL,
+        forma_pagamento VARCHAR NOT NULL,
         data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         CONSTRAINT fk_contas FOREIGN KEY (conta_id) REFERENCES contas (id)
     );
